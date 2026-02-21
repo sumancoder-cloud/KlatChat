276c1aca34e96f44c8a020b8a21e8c3e8b6998a4
Suman Yadav <your-email@example.com>
2026-02-21 23:29:21 +0530

    second Push

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express=require('express');
 const dotenv=require('dotenv');
 const app=express();
+const path=require('path')
 //even we can useimport express from 'express'  but need to keep the type as module
-
 const authRoutes=require('./routes/auth.route.js')
 const messageRoutes=require('./routes/auth.messages.js')
 const PORT=process.env.PORT || 3001
@@ -11,6 +11,15 @@ console.log("PORT from env:", process.env.PORT);
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes);
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.use((req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
+
+
 app.listen(PORT,()=>{
     console.log("Server is running on port number "+PORT);
 })
