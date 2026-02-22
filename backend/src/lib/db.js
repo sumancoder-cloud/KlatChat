8af0786746c559b20e6b13e4d881c41c975fde77
Suman Yadav <your-email@example.com>
2026-02-22 16:35:38 +0530

    signup part refactored successfully

---
+++ backend/src/lib/db.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+const connectDb=async ()=>{
+    
+    try{
+        const {MONGO_URI}=process.env;
+        if(!MONGO_URI) throw new error("Mongodb connection string was interrupted");
+        const conn=await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongo db connected Succesfully : ",conn.connection.host);
+    }
+    catch(error){
+        console.log("Mongo Db connection Failed",error.message);
+        process.exit(1); //here 1 means failure and 0 means success 
+    }
+}
+
+module.exports=connectDb;
